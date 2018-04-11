71d7f79bcd81ee072562cbf303a70eb9609affd9
Jason Chang <jasonsfchang@google.com>
2018-04-11 11:17:58 +0800

    SettingsLib: Update Utils APIs usage
    Update Utils.getColorAttr and similar methods in Utils to return a ColorStateList instead of a color
    
    Bug: 74872054
    Test: Force to display different color items then manually test
    Change-Id: I92697a597b532ba95f29a886baa114ed6d0ac632

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -210,7 +210,8 @@ private int mUser;
         updateResources();
 
         Rect tintArea = new Rect(0, 0, 0, 0);
-        int colorForeground = Utils.getColorAttr(getContext(), android.R.attr.colorForeground);
+        int colorForeground = Utils.getColorAttrDefaultColor(getContext(),
+                android.R.attr.colorForeground);
         float intensity = getColorIntensity(colorForeground);
         int fillColor = fillColorForIntensity(intensity, getContext());
 
@@ -703,7 +704,7 @@ private int mUser;
             mBatteryMeterView.onDarkChanged(new Rect(), 0, DarkIconDispatcher.DEFAULT_ICON_TINT);
         } else {
             Rect tintArea = new Rect(0, 0, 0, 0);
-            float colorIntensity = getColorIntensity(Utils.getColorAttr(getContext(), android.R.attr.colorForeground));
+            float colorIntensity = getColorIntensity(Utils.getColorAttrDefaultColor(getContext(), android.R.attr.colorForeground));
             int fillColorForIntensity = fillColorForIntensity(colorIntensity, getContext());
             mBatteryRemainingIcon.setColorsFromContext(mHost.getContext());
             mBatteryRemainingIcon.onDarkChanged(tintArea, colorIntensity, fillColorForIntensity);
